4265d6dc4a0c1bf4def07d05abd4df5716d376b5
npnutthapon <68525346+npnutthapon@users.noreply.github.com>
2020-10-02 20:03:43 +0700

    .

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col />
+      <v-col>
+        <v-img
+          src="https://cdn.discordapp.com/attachments/746260527235334237/761559406692270080/LogoMM.png"
+          class="img-fluid mx-auto"
+        />
+      </v-col>
+      <v-col />
+    </v-row>
+    <br>
+    <br>
+    <br>
+    <v-rov align="center" justify="center">
+      <h1>Welcome to iTunes Music. Play your music in a new way today!</h1>
+    </v-rov>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
+<style>
+  .theme--dark.v-application {
+  background-image: url('https://cdn.discordapp.com/attachments/746260527235334237/761540128077512704/dark-bg.jpg');
+  background-attachment: fixed;
+  background-position: 100% 100%;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
